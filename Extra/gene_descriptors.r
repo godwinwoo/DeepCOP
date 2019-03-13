662f4d51ecebe759cd5ddbcf10c80676202f027b
gwoo <godwinwoo@hotmail.com>
2019-03-13 14:29:54 -0700

    Update GO descriptors generation.

--- Extra/gene_descriptors.r
+++ Extra/gene_descriptors.r
@@ -1,40 +1,46 @@
-library(ontologyIndex)
 library(jsonlite)
 library(ontologySimilarity)
-library(RCurl)
 
-data(go)
-data(gene_GO_terms)
-data(GO_IC)
+data(gene_GO_terms) # Gene Ontology annotation of genes
+gene_json_data <- jsonlite::fromJSON('landmark_genes.json')
+gene_symbols <- gene_json_data["gene_symbol"][[1]]
+landmark_genes_GO_terms <- gene_GO_terms[gene_symbols]
 
-gene_json_data <- jsonlite::fromJSON('Data/landmark_genes.json')
-
-gene_list <- gene_json_data["gene_symbol"][[1]]
-
-beach <- gene_GO_terms[gene_list]
-
-#Go term selection top-5 based on information content (IC)
-go_terms <- c()
-for (i in (1:length(gene_list) )){
-  ic<-get_term_info_content(go,beach[[i]])[0:5]
-  go_selected <- beach[[i]][order(ic,decreasing = TRUE)]
-  #print(c(i,gene_list[[i]], go_selected))
-  go_terms <- c(go_terms, go_selected)
+# gather all go terms for the lm genes
+all_landmark_genes_go_terms_vector <- c()
+for (i in (1:length(gene_symbols) )){
+  go_selected <- landmark_genes_GO_terms[[i]]
+  all_landmark_genes_go_terms_vector <- c(all_landmark_genes_go_terms_vector, go_selected)
 }
-go_terms_unique <- sort(unique(go_terms))
+all_unique_landmark_genes_go_terms <- sort(unique(all_landmark_genes_go_terms_vector))
 
-#Descriptors binary occurency vector
-
-gene_go_fingerprint <- matrix(0,length(gene_list),length(go_terms_unique))
-for (i in (1:length(go_terms_unique))){
-  for (j in (1:length(gene_list))){
-    if (length(which(beach[[j]] == go_terms_unique[[i]])) == 1){
-      gene_go_fingerprint[j,i] <- 1
-      
+# get go terms that are attributed to by more than 3 landmark genes
+common_go_terms <- c()
+for (i in (1:length(all_unique_landmark_genes_go_terms))){
+  lm_go_term<-all_unique_landmark_genes_go_terms[[i]]
+  count <- 0
+  for (j in (1:length(gene_symbols))){
+    lm_gene_go_terms <- landmark_genes_GO_terms[[j]]
+    if (any(lm_gene_go_terms==lm_go_term)){
+      count <- count+1
+    }
+    if (count > 3){
+      common_go_terms <- c(common_go_terms, lm_go_term)
+      break
     }
   }
 }
 
-rownames(gene_go_fingerprint) <- gene_list
-colnames(gene_go_fingerprint) <- go_terms_unique
-write.csv(gene_go_fingerprint,file="Data/go_fingerprints.csv", quote=FALSE)
+# descriptors binary occurency vector
+gene_go_fingerprint <- matrix(0,length(gene_symbols),length(common_go_terms))
+for (i in (1:length(common_go_terms))){
+  for (j in (1:length(gene_symbols))){
+    if (length(which(landmark_genes_GO_terms[[j]] == common_go_terms[[i]])) == 1){
+      gene_go_fingerprint[j,i] <- 1
+    }
+  }
+}
+
+rownames(gene_go_fingerprint) <- gene_symbols
+colnames(gene_go_fingerprint) <- common_go_terms
+write.csv(gene_go_fingerprint,file="go_fingerprints.csv", quote=FALSE)
